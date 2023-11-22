4a94b82bea3fe4120cb0c01689bdc0bdcbe33b96
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2023-11-22 10:38:23 -0600

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,1 +1,17 @@
 
+
+hello1 = '**************************'
+hello2 = '*                        *'
+hello3 = '*        WELCOME!        *'
+
+# starting with empty line
+print('')
+print(hello1)
+print(hello2)
+print(hello3)
+print(hello2)
+print(hello1)
+
+
+
+
